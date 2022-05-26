61fc50732dbc47dd13810902a7a946226e0e508f
Tom Gregor <tgregor@ivsoftware.com>
2022-05-26 10:25:12 -0600

    Add project files.

---
+++ visibility_delay_without_blocking/MainForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace visibility_delay_without_blocking
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            new Thread(makeVisibleOnDelay).Start();
+        }
+
+        private void makeVisibleOnDelay()
+        {
+            Thread.Sleep(1000);
+            Invoke((MethodInvoker)delegate { panel1.Visible = true; });
+            Thread.Sleep(1000);
+            Invoke((MethodInvoker)delegate { panel2.Visible = true; });
+        }
+    }
+}
+
---
+++ visibility_delay_without_blocking/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace visibility_delay_without_blocking
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
